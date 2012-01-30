c0fb71bd04eb050e18ce18312cc70cb6853eda1e
Dieter Maas <dieter.maas@gmx.net>
2012-01-31 00:25:55 +0100

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
-  def home
-  end
-
-  def contact
-  end
-
-  def about
-  end
-
+ def home
+   @title = "Home"
+ end
+ def contact
+   @title = "Contact"
+ end
+ def about
+   @title = "About"
+ end
 end
