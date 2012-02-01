5ca606a46eac2146332e929ab9d04bace446e218
Dieter Maas <dieter.maas@gmx.net>
2012-02-01 23:50:00 +0100

    Finished layout and routes2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+ 
+ def logo
+# <a href="/"><img alt="Sample App" class="round" src="/images/logo.png" /></a>
+
+ end
+
  def title
    base_title = "Ruby on Rails diminishes spirituality"
    if @title.nil?
