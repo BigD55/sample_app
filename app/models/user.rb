ecd8e9747b9ca2ebb7b217ac103955de2fd965eb
Dieter Maas <dieter.maas@gmx.net>
2012-02-02 18:03:51 +0100

    Finished first cut of the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
+  attr_accessible :name, :email
+
+  
+  validates :name, :presence => true
+  validates :name, :presence => true,
+                     :length => { :maximum => 50 }
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, :presence => true,
+                     :format => { :with => email_regex },
+                     :uniqueness => { :case_sensitive => false }
 end
