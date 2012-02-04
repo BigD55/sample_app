a3a06037bc7ed5936b73813696d8b5dcec5b12e2
Dieter Maas <dieter.maas@gmx.net>
2012-02-04 20:03:56 +0100

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,3 +5,9 @@
      user.password "foobar"
      user.password_confirmation "foobar"
   end
+  Factory.sequence :name do |n|
+     "Person #{n}"
+  end
+  Factory.sequence :email do |n|
+     "person-#{n}@example.com"
+  end
