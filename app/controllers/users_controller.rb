1867d05cdbd8468f81bfd29234b219211c639eaf
Dieter Maas <dieter.maas@gmx.net>
2012-02-05 21:12:21 +0100

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   end
   def show
      @user = User.find(params[:id])
+     @microposts = @user.microposts.paginate(:page => params[:page])
      @title = @user.name
   end
   def new
@@ -45,9 +46,7 @@ class UsersController < ApplicationController
      redirect_to users_path
   end
   private
-     def authenticate
-         deny_access unless signed_in?
-     end
+    
      def correct_user
          @user = User.find(params[:id])
          redirect_to(root_path) unless current_user?(@user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
 
-  
+  has_many :microposts, :dependent => :destroy
   validates :name, :presence => true
   validates :name, :presence => true,
                      :length => { :maximum => 50 }
@@ -39,6 +39,10 @@ class User < ActiveRecord::Base
   def self.authenticate_with_salt(id, cookie_salt)
      user = find_by_id(id)
      (user && user.salt == cookie_salt) ? user : nil
+  end
+  def feed
+     # This is preliminary. See Chapter 12 for the full implementation.
+     Micropost.where("user_id = ?", id)
   end
 
   private
