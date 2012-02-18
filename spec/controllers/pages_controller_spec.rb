3ac8f9ce9a62050d24627537dd6028e61ac5b539
Dieter Maas <dieter.maas@gmx.net>
2012-02-18 19:56:22 +0100

    addedfollowing

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -16,7 +16,7 @@ describe PagesController do
         response.should be_success
       end
       it "should have the right title" do
-        response.should have_selector("title", :content => "#{@base_title} | Home")
+        response.should have_selector("title", :content => @base_title + "| Home")
       end
     end
     describe "when signed in" do
